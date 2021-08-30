cd17410443574993094646d176a2ac5298690a3f
Amandeep Singh <amandeep.bajwa@care.com>
2021-08-30 17:38:45 +0530

    feat: add endpoints, dockerfile and github action

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,13 +1,18 @@
 import sqlite3
+import logging
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+db_connection_count = 0
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global db_connection_count
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    db_connection_count += 1
     return connection
 
 # Function to get a post using its ID
@@ -22,7 +27,7 @@ def get_post(post_id):
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
-# Define the main route of the web application 
+# Define the main route of the web application
 @app.route('/')
 def index():
     connection = get_db_connection()
@@ -36,16 +41,19 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.info('Article not found')
       return render_template('404.html'), 404
     else:
+      app.logger.info('Article titled "%s" retrieved', post['title'])
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('"About Us" page retrieved')
     return render_template('about.html')
 
-# Define the post creation functionality 
+# Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
@@ -61,10 +69,38 @@ def create():
             connection.commit()
             connection.close()
 
+            app.logger.info('New Article titled "%s" created', title)
+
             return redirect(url_for('index'))
 
     return render_template('create.html')
+    
+# Define the healthz route of the web application
+@app.route('/healthz')
+def healthz():
+    response = app.response_class(
+        response=json.dumps({"result":"OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+    app.logger.info('Status request successfull')
+    return response
+
+# Define the metrics route of the web application
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    post_count = len(connection.execute('SELECT * FROM posts').fetchall())
+    connection.close()
+    app.logger.info('Metrics retrieval successful')
+    return jsonify({'db_connection_count': db_connection_count, 'post_count': post_count})
+
 
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+  logging.basicConfig(
+    format='%(levelname)s:%(name)s:%(asctime)s, %(message)s',
+    datefmt='%m/%d/%Y, %H:%M:%S',
+    level=logging.DEBUG
+  )
+  app.run(host='0.0.0.0', port='3111')
